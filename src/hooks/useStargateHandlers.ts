ca26bdc93b652d7f55d7b712a0cd24363926ed5e
Alexander Schiftner <alex@shapediver.com>
2025-09-26 11:10:11 +0200

    improved "get data" handler

--- src/hooks/useStargateHandlers.ts
+++ src/hooks/useStargateHandlers.ts
@@ -12,13 +12,33 @@ import {
 import {useCallback} from "react";
 import {SessionData} from "./useShapeDiverStargate";
 
-const exampleJsonFile = new Blob([JSON.stringify({hello: "world"}, null, 2)], {
-	type: "application/json",
-});
+/**
+ * Example files that can be used by the handlers.
+ */
+const exampleFiles: {filename: string; contentType: string; href: string}[] = [
+	{
+		filename: "test.json",
+		contentType: "application/json",
+		href: window.location.origin + "/test.json",
+	},
+	{
+		filename: "test.3dm",
+		contentType: "model/vnd.3dm",
+		href: window.location.origin + "/test.3dm",
+	},
+	{
+		filename: "test.dwg",
+		contentType: "application/dwg",
+		href: window.location.origin + "/test.dwg",
+	},
+];
 
+/**
+ * Which content types, file extensions, and parameter types are supported by the handlers.
+ */
 const supportedData: Partial<ISdStargateGetSupportedDataReplyDto> = {
-	contentTypes: ["application/json"],
-	fileExtensions: ["json"],
+	contentTypes: ["application/json", "application/dwg", "model/vnd.3dm"],
+	fileExtensions: ["json", "3dm", "dwg"],
 	parameterTypes: ["File"],
 };
 
@@ -32,7 +52,7 @@ export default function useStargateHandlers(): {
 	 */
 	supportedData: Partial<ISdStargateGetSupportedDataReplyDto>;
 	/**
-	 * Handler for the get data command.
+	 * Handler for the GET DATA command.
 	 * @param data
 	 * @param sessionData
 	 * @returns
@@ -42,7 +62,7 @@ export default function useStargateHandlers(): {
 		sessionData: SessionData,
 	) => Promise<ISdStargateGetDataReplyDto>;
 	/**
-	 * Handler for the bake data command.
+	 * Handler for the BAKE DATA command.
 	 * @param data
 	 * @param sessionData
 	 * @returns
@@ -52,7 +72,7 @@ export default function useStargateHandlers(): {
 		sessionData: SessionData,
 	) => Promise<ISdStargateBakeDataReplyDto>;
 	/**
-	 * Handler for the export file command.
+	 * Handler for the EXPORT FILE command.
 	 * @param data
 	 * @param sessionData
 	 * @returns
@@ -62,7 +82,7 @@ export default function useStargateHandlers(): {
 		sessionData: SessionData,
 	) => Promise<ISdStargateExportFileReplyDto>;
 } {
-	// example handler for the get data command
+	// example handler for the GET DATA command
 	const getDataCommandHandler = useCallback(
 		async (
 			{parameter: {id: parameterId}}: ISdStargateGetDataCommandDto,
@@ -72,26 +92,37 @@ export default function useStargateHandlers(): {
 			const paramDef = session.parameters![parameterId];
 			// in this example we handle "File" parameters with "application/json" format
 			if (paramDef.type === "File") {
-				if (paramDef.format?.includes("application/json")) {
+				// try to find a suitable test file
+				const testFile = exampleFiles.find((f) =>
+					paramDef.format?.includes(f.contentType),
+				);
+				if (testFile) {
+					// download the file
+					const downloadedFile = await new UtilsApi(config).download(
+						testFile.href,
+						{responseType: "arraybuffer"},
+					);
 					// request a file upload URL from the Geometry API
 					const response = await new FileApi(config).uploadFile(
 						session.sessionId,
 						{
 							[parameterId]: {
-								size: exampleJsonFile.size,
-								filename: "example.json",
-								format: "application/json",
+								size: (
+									downloadedFile.data as unknown as ArrayBuffer
+								).byteLength,
+								filename: testFile.filename,
+								format: testFile.contentType,
 							},
 						},
 					);
 					// extract the data for the file to be uploaded
 					const fileData = response.data.asset.file[parameterId];
-					// upload the file
+					// upload the file to the URL provided by the Geometry API
 					await new UtilsApi(config).upload(
 						fileData.href,
-						exampleJsonFile,
-						"application/json",
-						"example.json",
+						downloadedFile.data,
+						testFile.contentType,
+						testFile.filename,
 					);
 					// return the id of the uploaded file
 					return {
