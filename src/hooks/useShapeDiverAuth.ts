664f2ca36442dc662e090e255368c5451b44b616
Alexander Schiftner <alex@shapediver.com>
2026-01-08 11:28:43 +0100

    fix client id

--- src/hooks/useShapeDiverAuth.ts
+++ src/hooks/useShapeDiverAuth.ts
@@ -18,7 +18,7 @@ const authBaseUrl =
 		? "https://staging-wwwcdn.us-east-1.shapediver.com"
 		: "https://www.shapediver.com";
 const authEndPoint = `${authBaseUrl}/oauth/authorize`;
-const clientId = "A085FCC5-6EEB-46A6-A381-ADCEDB6E59D6"; // "660310c8-50f4-4f47-bd78-9c7ede8e659b";
+const clientId = "660310c8-50f4-4f47-bd78-9c7ede8e659b";
 
 async function sha256(buffer: Uint8Array<ArrayBuffer>): Promise<ArrayBuffer> {
 	return await crypto.subtle.digest("SHA-256", buffer);
